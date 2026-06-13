6c20fb9a2323323e44130e8140cdcd81b6f9737e
tyrannosaurusdm92 <tyrannosaurus.dm92@gmail.com>
2026-06-12 22:17:26 -0400

    Delete js directory

--- js/emperor-onyx-rulebot-data.js
+++
@@ -1,7 +0,0 @@
-// Compatibility shim for older Onyx Helper Bot code.
-// The merged Squishy + Onyx data now lives in js/onyx-personality-data.js.
-(function(){
-  'use strict';
-  window.EMPEROR_ONYX_PERSONALITY_DATA = window.EMPEROR_ONYX_PERSONALITY_DATA || {};
-  window.EMPEROR_ONYX_RULEBOT_DATA = window.EMPEROR_ONYX_PERSONALITY_DATA;
-})();
