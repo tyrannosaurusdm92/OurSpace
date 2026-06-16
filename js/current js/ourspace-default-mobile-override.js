23447aab8e80e4757de22cde17f25fd266ad6aea
tyrannosaurusdm92 <tyrannosaurus.dm92@gmail.com>
2026-06-16 00:46:34 -0400

    Delete js/current js directory

--- js/current js/ourspace-default-mobile-override.js
+++
@@ -1,4 +0,0 @@
-/* OurSpace default mobile override placeholder.
-   Replace this file with a #admin-editor export to make your manually arranged mobile layout the default. */
-window.OurSpaceDefaultLayoutOverrides = window.OurSpaceDefaultLayoutOverrides || {};
-window.OurSpaceDefaultLayoutOverrides.mobile = window.OurSpaceDefaultLayoutOverrides.mobile || { version:'placeholder', mode:'mobile', modules:{} };
